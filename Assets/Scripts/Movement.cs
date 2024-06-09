c3ce996889a4f7988b8635f351ea2fc6abfade68
ItzYaboiiJoe <josephakseoudy@gmail.com>
2024-06-09 18:13:41 -0400

    Fixed Movement and added Score System

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,75 +10,60 @@ public class Movement : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float groundDist = 0.25f;
     [SerializeField] private Transform pos;
-    [SerializeField] private float jumpTime = 0.3f;
-
-    [SerializeField] private float crouchHeight = 0.5f;
+    [SerializeField] private float jumpTime = 0.2f;
+    [SerializeField] private float crouchHeightFactor = 0.5f;
 
     private bool isGrounded = false;
-    private bool isjumping = false;
+    public bool isJumping = false;
     private float jumpTimer;
 
+    private Vector3 originalScale;
 
-
-    //This code is now broken to when is key is tapped it cannot be pressed again. Attempting to debug the code here. 
+    private void Start()
+    {
+        originalScale = sprite.localScale;
+    }
 
     private void Update()
     {
         isGrounded = Physics2D.OverlapCircle(pos.position, groundDist, groundLayer);
 
-
-
         #region JUMPING
-
-
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
-            isjumping = true;
-          //  jumpTimer = 0f; //This is suppose to reset the jumptimer so player can jump again
+            isJumping = true;
             rb.velocity = Vector2.up * jumpForce;
+            jumpTimer = 0;
         }
 
-        if (Input.GetButton("Jump"))
+        if (isJumping && Input.GetButton("Jump"))
         {
             if (jumpTimer < jumpTime)
             {
                 rb.velocity = Vector2.up * jumpForce;
-
                 jumpTimer += Time.deltaTime;
             }
             else
             {
-                isjumping = false;
-            }    
+                isJumping = false;
+            }
         }
-
 
         if (Input.GetButtonUp("Jump"))
         {
-            isjumping = false;
+            isJumping = false;
         }
-        
-
         #endregion
 
-
-    //THIS FEATURE IS BREAKING THE CONTROL SETUP. I need to debug and fix.
+        #region CROUCHING
         if (isGrounded && Input.GetButton("Crouch"))
         {
-            sprite.localScale = new Vector3(sprite.localScale.x, crouchHeight, sprite.localScale.z);
-
-
-            if (isjumping){
-                sprite.localScale = new Vector3(sprite.localScale.x, 1f, sprite.localScale.z);
-            }
-           
-
+            sprite.localScale = new Vector3(originalScale.x, originalScale.y * crouchHeightFactor, originalScale.z);
         }
         if (Input.GetButtonUp("Crouch"))
         {
-            sprite.localScale = new Vector3(sprite.localScale.x, 1f, sprite.localScale.z);
+            sprite.localScale = originalScale;
         }
-        
+        #endregion
     }
-
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    public Text highScoreText;
+
+    public float scoreCount;
+    public float highScoreCount;
+
+    public float pointsPerSecond;
+
+    public bool scoreIncreasing;
+
+    void Update()
+    {
+        if (scoreIncreasing)
+        {
+            scoreCount += pointsPerSecond * Time.deltaTime;
+        }
+
+        if (scoreCount > highScoreCount)
+        {
+            highScoreCount = scoreCount;
+        }
+
+        scoreText.text = "Score: " + (int)scoreCount;
+        highScoreText.text = "High Score : " + (int)highScoreCount;
+    }
+}
