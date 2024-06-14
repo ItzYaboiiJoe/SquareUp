cd4f779f362e6847eda9953e03f6b556a3dea222
Engulfedleader2 <engulfedleader@gmail.com>
2024-06-14 19:23:24 -0400

    Player Controller
    Player controller has been  made and works :LD

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    //this allow variable to be adjusted in settings
+   [SerializeField] public float jumpForce = 50f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGrounded && Input.GetButtonDown("Jump"))
+        {
+            //JUMP COMMAND
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity =  new Vector2(rb.velocity.x, jumpForce);
+        isGrounded = false;
+    }
+    //check if player o nground based on layer asset
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.contacts[0].normal.y > 0.5f)
+        {
+            isGrounded = true;
+        }
+    }
+}
