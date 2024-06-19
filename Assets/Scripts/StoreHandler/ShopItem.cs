2c9417f5c387df4e1a207ea19279f1cf1174f6ad
Engulfedleader2 <engulfedleader@gmail.com>
2024-06-19 00:56:20 -0400

    Store UI Created
    store UI is created. currently working on the scrips for in game purchases

---
+++ Assets/Scripts/StoreHandler/ShopItem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "shopMenu", menuName = "Scriptable Objects/new Shop Item", order = 1)]
+public class ShopItem : ScriptableObject
+{
+    public string title;
+    public Image skinImage;
+    public string description;
+    public int cost;
+}
