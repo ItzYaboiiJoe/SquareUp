b9361538f7dd05600512c500301454134e90fb25
Engulfedleader2 <engulfedleader@gmail.com>
2024-06-20 19:48:03 -0400

    asset uploaded / fixed canvas
    updated canvas isze to be compatible on all devices
    new assets are uplaodded for testing
    working on the playercontroller script to fix the crouch bug and add swping commnands

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Numerics;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float parallaxEffectMultiplier;
+    private float length;
+    private UnityEngine.Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * parallaxEffectMultiplier, length);
+        transform.position = startPos + UnityEngine.Vector3.left * newPosition;
+    }
+}
