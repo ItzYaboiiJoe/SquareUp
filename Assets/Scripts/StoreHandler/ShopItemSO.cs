7b7cd417b4d3d759b5ab59db0824f150cb5a5405
Engulfedleader2 <engulfedleader@gmail.com>
2024-06-24 17:57:09 -0400

    shop asset fix
    Shop assets created

--- Assets/Scripts/StoreHandler/ShopItemSO.cs
+++ Assets/Scripts/StoreHandler/ShopItemSO.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-[CreateAssetMenu(fileName = "shopMenu", menuName = "Scriptable Objects/new Shop Item", order = 1)]
+
+[CreateAssetMenu(fileName = "newShopItem", menuName = "Scriptable Objects/new Shop Item", order = 1)]
 public class ShopItemSO : ScriptableObject
 {
     public string title;
