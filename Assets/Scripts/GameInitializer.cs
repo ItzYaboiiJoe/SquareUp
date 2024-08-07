91b0fdf701c2def24a2be52f2695e07c4295b729
Engulfedleader2 <engulfedleader@gmail.com>
2024-08-07 13:11:06 -0400

    andoir base build

--- Assets/Scripts/GameInitializer.cs
+++ Assets/Scripts/GameInitializer.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-#if !UNITY_WEBGL
-using Firebase;
-using Firebase.Extensions;
-#endif
+// #if !UNITY_WEBGL
+// using Firebase;
+// using Firebase.Extensions;
+// #endif
 
 public class GameInitializer : MonoBehaviour
 {
@@ -24,9 +24,9 @@ public class GameInitializer : MonoBehaviour
 
         PlayerPrefs.Save();
 
-#if !UNITY_WEBGL
-        InitializeFirebase();
-#endif
+// #if !UNITY_WEBGL
+//         InitializeFirebase();
+// #endif
     }
 
     private void Start()
@@ -34,24 +34,24 @@ public class GameInitializer : MonoBehaviour
         Application.targetFrameRate = 60;
     }
 
-#if !UNITY_WEBGL
-    void InitializeFirebase()
-    {
-        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
-        {
-            var dependencyStatus = task.Result;
-            if (dependencyStatus == DependencyStatus.Available)
-            {
-                // Firebase is ready to use
-                Debug.Log("Firebase is ready to use.");
-            }
-            else
-            {
-                Debug.LogError($"Could not resolve all Firebase dependencies: {dependencyStatus}");
-                // Firebase Unity SDK is not safe to use here.
-            }
-        });
-    }
-#endif
+// #if !UNITY_WEBGL
+//     void InitializeFirebase()
+//     {
+//         FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+//         {
+//             var dependencyStatus = task.Result;
+//             if (dependencyStatus == DependencyStatus.Available)
+//             {
+//                 // Firebase is ready to use
+//                 Debug.Log("Firebase is ready to use.");
+//             }
+//             else
+//             {
+//                 Debug.LogError($"Could not resolve all Firebase dependencies: {dependencyStatus}");
+//                 // Firebase Unity SDK is not safe to use here.
+//             }
+//         });
+//     }
+// #endif
 
 }
